03ebb3083f9ed9790c801b15e844dfe13c1f4ad9
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2018-11-26 18:11:13 +0800

    Added type_support/promote.hpp.
    promote class template - provides member type equivalent to applying integer promotion to a type.
    promote_t alias template - alias for member type of promote.

---
+++ include/tl/type_support/promote.hpp
@@ -0,0 +1,39 @@
+#ifndef TL_TYPE_SUPPORT_PROMOTE_HPP
+#define TL_TYPE_SUPPORT_PROMOTE_HPP
+
+
+#include <tc/type_support/is_unscoped_enum.hpp>		// tc::type_support::is_unscoped_enum_v
+#include <type_traits>								// std::enable_if_t, std::is_integral_v
+#include <utility>									// std::declval
+
+
+namespace tl::type_support {
+	
+	namespace detail {
+		
+		// Matches non-promotable types.
+		template<typename T, typename = void>
+		struct promote_base {
+			using type = T;
+		};
+		
+		// Matches promotable types.
+		template<typename T>
+		struct promote_base<T, std::enable_if_t<std::is_integral_v<T> || is_unscoped_enum_v<T>, void>> {
+			using type = decltype(+std::declval<T>());
+		};
+		
+	}
+
+	// Provides member type alias type which is the integer promoted T if T may undergo integer promotion, otherwise is T.
+	template<typename T>
+	struct promote : detail::promote_base<T> {};
+
+
+	template<typename T>
+	using promote_t = typename promote<T>::type;
+	
+}
+
+
+#endif
