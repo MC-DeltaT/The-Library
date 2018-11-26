3a9d8286bf6335a946c331bb83d7c1c60dc6b9f1
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2018-11-26 17:26:23 +0800

    Added type_support/is_int.hpp.
    is_int class template - provides member bool indicating whether a type is a modification of int.
    is_int_v variable template - equal to member value of is_int.

---
+++ include/tl/type_support/is_int.hpp
@@ -0,0 +1,33 @@
+#ifndef TL_TYPE_SUPPORT_IS_INT_HPP
+#define TL_TYPE_SUPPORT_IS_INT_HPP
+
+
+#include <tc/type_support/is_any_of.hpp>		// tc::type_support::is_any_of
+#include <type_traits>							// std::remove_cv_t
+
+
+namespace tl::type_support {
+	
+	namespace detail {
+		
+		// `std::true_type` if `T` is `int` or any size/signedness modification thereof, otherwise std::false_type.
+		template<typename T>
+		struct is_int_base : is_any_of<T, short, int, long, long long,
+			unsigned short, unsigned, unsigned long, unsigned long long> {};
+		
+	}
+	
+	
+	/* std::true_type if T is int or any cv-qualified, size/signedness modification thereof, otherwise std::false_type.
+		Eg. std::true_type for int, const int, short, volatile short, unsigned, const unsigned, etc. */
+	template<typename T>
+	struct is_int : detail::is_int_base<std::remove_cv_t<T>> {};
+
+
+	template<typename T>
+	inline constexpr bool is_int_v = is_int<T>::value;
+	
+}
+
+
+#endif
