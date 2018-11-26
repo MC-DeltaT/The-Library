eb7091e3e3a5495f6e8e9e4226453a24bc69f5e4
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2018-11-26 17:03:42 +0800

    Added type_support/add_pointers.hpp.
    add_pointers class template - provides member type alias adding N levels of pointers to T.
    add_pointers_t alias template - alias for member type of add_pointers.

---
+++ include/tl/type_support/add_pointers.hpp
@@ -0,0 +1,28 @@
+#ifndef TL_TYPE_SUPPORT_ADD_POINTERS_HPP
+#define TL_TYPE_SUPPORT_ADD_POINTERS_HPP
+
+
+#include <cstddef>			// std::size_t
+#include <type_traits>		// std::add_pointer
+
+
+namespace tl::type_support {
+	
+	// Provides member type alias type which is equivalent to adding N levels of pointers to T.
+	template<typename T, std::size_t N>
+	struct add_pointers : std::add_pointer<typename add_pointers<T, N - 1>::type> {};
+
+	// Specialization for adding zero levels of pointers; type is equivalent to T.
+	template<typename T>
+	struct add_pointers<T, 0> {
+		using type = T;
+	};
+	
+
+	template<typename T, std::size_t N>
+	using add_pointers_t = typename add_pointers<T, N>::type;
+	
+}
+
+
+#endif
