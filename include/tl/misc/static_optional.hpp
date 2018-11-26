c8ec05cf955ca76d9aa1cb6a5fcc3b462bffd5e5
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2018-11-26 18:17:36 +0800

    Added misc/static_optional.hpp.
    static_optional class template - contains or doesn't contain an object based on compile-time boolean.

---
+++ include/tl/misc/static_optional.hpp
@@ -0,0 +1,19 @@
+#ifndef TL_MISC_STATIC_OPTIONAL_HPP
+#define TL_MISC_STATIC_OPTIONAL_HPP
+
+
+namespace tl::misc {
+	
+	// Has member variable value if B is true, otherwise has no member.
+	template<bool B, typename T>
+	struct static_optional {};
+	
+	template<typename T>
+	struct static_optional<true, T> {
+		T value;
+	};
+	
+}
+
+
+#endif
