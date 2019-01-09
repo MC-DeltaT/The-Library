89850ed409e2238fc8e88a527d7e9e880df83a55
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2019-01-09 23:39:23 +0800

    Added ranges/range_traits.hpp.
    range_traits class template - provides type traits of a range type.

---
+++ include/tl/ranges/range_traits.hpp
@@ -0,0 +1,23 @@
+#ifndef TL_RANGES_RANGE_TRAITS_HPP
+#define TL_RANGES_RANGE_TRAITS_HPP
+
+
+#include <iterator>			// std::begin, std::end, std::iterator_traits
+#include <utility>			// std::declval
+
+
+namespace tl::ranges {
+
+	// Provides the type traits of a range type.
+	template<typename Range>
+	struct range_traits {
+		using iterator = decltype(std::begin(std::declval<Range>()));
+		using sentinel = decltype(std::end(std::declval<Range>()));
+		using value_type = typename std::iterator_traits<iterator>::value_type;
+		using reference = typename std::iterator_traits<iterator>::reference;
+	};
+
+}
+
+
+#endif
