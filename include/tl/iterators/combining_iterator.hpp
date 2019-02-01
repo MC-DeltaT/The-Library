3d3f5089f7ac6fd1279cd8d20089bc631776d5bf
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2019-02-01 14:18:18 +0800

    Added iterators/combining_iterator.hpp.
    combining_iterator class template - iterator adaptor that iterates multiple iterators at once and combines dereferenced values with a function.
    std::iterator_traits specialization - for combining_iterator.

---
+++ include/tl/iterators/combining_iterator.hpp
@@ -0,0 +1,94 @@
+#ifndef TL_ITERATORS_COMBINING_ITERATOR_HPP
+#define TL_ITERATORS_COMBINING_ITERATOR_HPP
+
+
+#include <iterator>										// std::iterator_traits
+#include <tl/iterators/transforming_iterator.hpp>		// tl::iterators::transforming_iterator
+#include <tl/iterators/zipping_iterator.hpp>			// tl::iterators::zipping_iterator
+#include <tuple>										// std::apply
+#include <utility>										// std::declval
+
+
+namespace tl::iterators {
+
+	namespace detail {
+
+		/* Creates a combining iterator from a zipping_iterator inside a transforming_iterator, which is then forwarded formally by combining_iterator.
+			op is taken and used by reference; the function object itself resides in the wrapping instance of combining_iterator. */
+		template<typename Operation, typename... Iterators>
+		auto make_combining_iterator_impl(Operation& op, Iterators... iterators)
+		{
+			// Need to inject a usage of std::apply to unpack the zipping_iterator's output tuple into arguments for the user-provided combiner function.
+			return transforming_iterator(zipping_iterator(iterators...), [&op](auto deref_tuple) {
+					return std::apply(op, deref_tuple);
+				});
+		}
+
+		template<typename Operation, typename... Iterators>
+		using combining_iterator_impl = decltype(make_combining_iterator_impl(std::declval<Operation>(), std::declval<Iterators>()...));
+
+	}
+
+
+	/* Iterator adaptor that iterates multiple iterators simultaneously and combines the iterates values with a function.
+		For simplicity this iterator inherits from tranforming_iterator. Its interface and supported operations are indentical except for the public
+		constructs explicitly declared here. */
+	template<typename Operation, typename... Iterators>
+	class combining_iterator : public detail::combining_iterator_impl<Operation, Iterators...> {
+	private:
+		// The transforming_iterator type from which this class inherits and which contains the majority of the implementation.
+		using _impl_type = detail::combining_iterator_impl<Operation, Iterators...>;
+
+	public:
+		/* Special members */
+
+		// Value-initializes the base iterators and function object.
+		combining_iterator() :
+			_op(),
+			_impl_type()
+		{}
+
+		// Constructs the base iterators and function object from the given values.
+		explicit combining_iterator(Operation op, Iterators... iterators) :
+			_op(op),
+			_impl_type(detail::make_combining_iterator_impl(_op, iterators...))
+		{}
+
+
+		/* General functions */
+
+		// Gets a tuple of the base iterators.
+		std::tuple<Iterators...> const& base() const
+		{
+			return static_cast<_impl_type const&>(*this).base().base();
+		}
+
+		// Gets the combiner function.
+		Operation const& operation() const
+		{
+			return _op;
+		}
+
+
+	private:
+		/* Variables */
+
+		// Need to hold onto the combiner function separately such that it can be reacquired.
+		Operation _op;
+	};
+
+}
+
+
+// Specialization for tl::iterators::combining_iterator.
+template<typename Operation, typename... Iterators>
+struct std::iterator_traits<tl::iterators::combining_iterator<Operation, Iterators...>> {
+	using value_type = typename tl::iterators::combining_iterator<Operation, Iterators...>::value_type;
+	using reference = typename tl::iterators::combining_iterator<Operation, Iterators...>::reference;
+	using pointer = void;
+	using difference_type = typename tl::iterators::combining_iterator<Operation, Iterators...>::difference_type;
+	using iterator_category = typename tl::iterators::combining_iterator<Operation, Iterators...>::iterator_category;
+};
+
+
+#endif
