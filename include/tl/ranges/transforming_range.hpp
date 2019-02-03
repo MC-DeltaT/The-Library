86e0805d81e2d0dc515698b5933b62389f17303f
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2019-02-03 22:30:58 +0800

    Added ranges/transforming_range.hpp.
    transforming_range class template - range adaptor that transforms elements with a function.

---
+++ include/tl/ranges/transforming_range.hpp
@@ -0,0 +1,93 @@
+#ifndef TL_RANGES_TRANSFORMING_RANGE_HPP
+#define TL_RANGES_TRANSFORMING_RANGE_HPP
+
+
+#include <iterator>										// std::begin, std::end
+#include <tl/iterators/transforming_iterator.hpp>		// tl::iterators::transforming_iterator
+#include <tl/ranges/range_traits.hpp>					// tl::ranges::range_traits
+
+
+namespace tl::ranges {
+
+	// Range adaptor that transforms elements with a function.
+	template<class Range, typename UnaryOperation>
+	class transforming_range {
+	public:
+		/* Member types */
+
+		using range_type = Range;
+		using iterator = iterators::transforming_iterator<typename range_traits<Range>::iterator, UnaryOperation>;
+		using sentinel = iterators::transforming_iterator<typename range_traits<Range>::sentinel, UnaryOperation>;
+
+
+		/* Special members */
+
+		// Destructs the base range and transformer function object.
+		~transforming_range() = default;
+
+		// Value-initializes the base range and transformer function object.
+		transforming_range() :
+			_base(),
+			_op()
+		{}
+
+		// Copy-constructs the base range and transformer function object from those of other.
+		transforming_range(transforming_range const& other) = default;
+
+		// Move-constructs the base range and transformer function object from those of other.
+		transforming_range(transforming_range&& other) = default;
+
+		// Constructs the base range and transformer function object from those of other.
+		transforming_range(Range base, UnaryOperation op) :
+			_base(base),
+			_op(op)
+		{}
+
+
+		/* Operators */
+
+		// Copy-assigns the base range and transformer function object from those of rhs.
+		transforming_range& operator=(transforming_range const& rhs) = default;
+
+		// Move-assigns the base range and transformer function object from those of rhs.
+		transforming_range& operator=(transforming_range&& rhs) = default;
+
+
+		/* General functions */
+
+		// Gets the base range.
+		Range const& base() const
+		{
+			return _base;
+		}
+
+		// Gets an iterator to the start of the transformed range.
+		iterator begin() const
+		{
+			return {std::begin(_base), _op};
+		}
+
+		// Gets a sentinel to the end of the transformed range.
+		sentinel end() const
+		{
+			return {std::end(_base), _op};
+		}
+
+		// Gets the transformer function.
+		UnaryOperation const& operation() const
+		{
+			return _op;
+		}
+
+
+	private:
+		/* Variables */
+
+		Range _base;
+		UnaryOperation _op;
+	};
+
+}
+
+
+#endif
