24c7e8201aad8ea0d516c439639650eb2a9c5725
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2019-02-19 01:33:26 +0800

    Fixed transform to forward tuple value category and constness correctly.

--- include/tl/tuple/transform.hpp
+++ include/tl/tuple/transform.hpp
@@ -2,10 +2,11 @@
 #define TL_TUPLE_TRANSFORM_HPP
 
 
-#include <cstddef>			// std::size_t
-#include <tuple>			// std::get, std::tuple, std::tuple_element_t, std::tuple_size_v
-#include <type_traits>		// std::invoke_result_t
-#include <utility>			// std::index_sequence, std::make_index_sequence
+#include <cstddef>								// std::size_t
+#include <tl/type_support/remove_cvref.hpp>		// tl::type_support::remove_cvref_t
+#include <tuple>								// std::get, std::tuple, std::tuple_size_v
+#include <type_traits>							// std::invoke_result_t
+#include <utility>								// std::forward, std::index_sequence, std::make_index_sequence
 
 
 namespace tl::tuple {
@@ -13,20 +14,21 @@ namespace tl::tuple {
 	namespace detail {
 
 		template<class Tuple, typename UnaryOperation, std::size_t... Idx>
-		auto transform_impl(Tuple const& tuple, UnaryOperation op, std::index_sequence<Idx...> idx)
+		auto transform_impl(Tuple&& tuple, UnaryOperation op, std::index_sequence<Idx...> idx)
 		{
-			using result_tuple = std::tuple<std::invoke_result_t<UnaryOperation, std::tuple_element_t<Idx, Tuple>>...>;
 			using std::get;
-			return result_tuple(op(get<Idx>(tuple))...);
+			using result_tuple = std::tuple<std::invoke_result_t<UnaryOperation, decltype(get<Idx>(std::forward<Tuple>(tuple)))>...>;
+			return result_tuple(op(get<Idx>(std::forward<Tuple>(tuple)))...);
 		}
 
 
 		template<class Tuple1, class Tuple2, typename BinaryOperation, std::size_t... Idx>
-		auto transform_impl(Tuple1 const& tuple1, Tuple2 const& tuple2, BinaryOperation op, std::index_sequence<Idx...> idx)
+		auto transform_impl(Tuple1&& tuple1, Tuple2&& tuple2, BinaryOperation op, std::index_sequence<Idx...> idx)
 		{
-			using result_tuple = std::tuple<std::invoke_result_t<BinaryOperation, std::tuple_element_t<Idx, Tuple1>, std::tuple_element_t<Idx, Tuple2>>...>;
 			using std::get;
-			return result_tuple(op(get<Idx>(tuple1), get<Idx>(tuple2))...);
+			using result_tuple = std::tuple<std::invoke_result_t<BinaryOperation,
+				decltype(get<Idx>(std::forward<Tuple1>(tuple1))), decltype(get<Idx>(std::forward<Tuple2>(tuple2)))>...>;
+			return result_tuple(op(get<Idx>(std::forward<Tuple1>(tuple1)), get<Idx>(std::forward<Tuple2>(tuple2)))...);
 		}
 
 	}
@@ -35,9 +37,10 @@ namespace tl::tuple {
 	/* Transforms each element of a tuple with a function and returns the results in a new tuple.
 		Works for any tuple type supported by std::get, std::tuple_element and std::tuple_size, but the return type is always std::tuple. */
 	template<class Tuple, typename UnaryOperation>
-	auto transform(Tuple const& tuple, UnaryOperation op)
+	auto transform(Tuple&& tuple, UnaryOperation op)
 	{
-		return detail::transform_impl(tuple, op, std::make_index_sequence<std::tuple_size_v<Tuple>>());
+		constexpr static auto idx = std::make_index_sequence<std::tuple_size_v<type_support::remove_cvref_t<Tuple>>>();
+		return detail::transform_impl(std::forward<Tuple>(tuple), op, idx);
 	}
 
 
@@ -45,9 +48,10 @@ namespace tl::tuple {
 		The two tuples must have the same number of elements.
 		Works for any tuple types supported by std::get, std::tuple_element and std::tuple_size, but the return type is always std::tuple. */
 	template<class Tuple1, class Tuple2, typename BinaryOperation>
-	auto transform(Tuple1 const& tuple1, Tuple2 const& tuple2, BinaryOperation op)
+	auto transform(Tuple1&& tuple1, Tuple2&& tuple2, BinaryOperation op)
 	{
-		return detail::transform_impl(tuple1, tuple2, op, std::make_index_sequence<std::tuple_size_v<Tuple1>>());
+		constexpr static auto idx = std::make_index_sequence<std::tuple_size_v<type_support::remove_cvref_t<Tuple1>>>();
+		return detail::transform_impl(std::forward<Tuple1>(tuple1), std::forward<Tuple2>(tuple2), op, idx);
 	}
 
 }
