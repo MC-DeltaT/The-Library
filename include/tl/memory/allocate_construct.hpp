09a73eb69f31037b0f96e40d95e846da52984e83
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2019-03-06 21:31:27 +0800

    Added memory/allocate_construct.hpp.
    allocate_construct function template - allocates memory for and constructs an object.

---
+++ include/tl/memory/allocate_construct.hpp
@@ -0,0 +1,23 @@
+#ifndef TL_MEMORY_ALLOCATE_CONSTRUCT_HPP
+#define TL_MEMORY_ALLOCATE_CONSTRUCT_HPP
+
+
+#include <memory>		// std::allocator_traits
+#include <utility>		// std::forward
+
+
+namespace tl::memory {
+
+	// Allocates memory for and constructs one object using the given allocator and constructor arguments. Returns a pointer to the object.
+	template<class Allocator, typename... Args>
+	typename std::allocator_traits<Allocator>::pointer allocate_construct(Allocator& alloc, Args&&... args)
+	{
+		typename std::allocator_traits<Allocator>::pointer ptr = std::allocator_traits<Allocator>::allocate(alloc, 1);
+		std::allocator_traits<Allocator>::construct(alloc, ptr, std::forward<Args>(args)...);
+		return ptr;
+	}
+
+}
+
+
+#endif
