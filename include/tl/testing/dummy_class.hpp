a37b40a7dbd1ed202782875448e0e710912c2374
Reece Jones (LAPTOP-DOTOC9S) <reece.jones131@gmail.com>
2019-03-07 09:52:31 +0800

    Added testing/dummy_class.hpp.
    dummy_class class - prints to the console on calling of constructors, operators, etc.

---
+++ include/tl/testing/dummy_class.hpp
@@ -0,0 +1,52 @@
+#ifndef TL_TESTING_DUMMY_CLASS_HPP
+#define TL_TESTING_DUMMY_CLASS_HPP
+
+
+#include <iostream>		// std::cout, std::endl
+
+
+namespace tl::testing {
+
+	struct dummy_class {
+		/* Special members */
+
+		~dummy_class()
+		{
+			std::cout << "Destructor" << std::endl;
+		}
+
+		dummy_class()
+		{
+			std::cout << "Default constructor" << std::endl;
+		}
+
+		dummy_class(dummy_class const&)
+		{
+			std::cout << "Copy constructor" << std::endl;
+		}
+
+		dummy_class(dummy_class&&)
+		{
+			std::cout << "Move constructor" << std::endl;
+		}
+
+
+		/* Operators */
+
+		dummy_class& operator=(dummy_class const&)
+		{
+			std::cout << "Copy assignment" << std::endl;
+			return *this;
+		}
+
+		dummy_class& operator=(dummy_class&&)
+		{
+			std::cout << "Move assignment" << std::endl;
+			return *this;
+		}
+	};
+
+}
+
+
+#endif
